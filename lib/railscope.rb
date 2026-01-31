9e92e6b92d865d871ebcda388380fa552d2fc0cd
Phelipe Tussolini <phelipe@taller.net.br>
2026-01-30 21:52:00 -0300

    Add  method to check feature toggle

--- lib/railscope.rb
+++ lib/railscope.rb
@@ -5,4 +5,8 @@ require_relative "railscope/engine"
 
 module Railscope
   class Error < StandardError; end
+
+  def self.enabled?
+    %w[true 1].include?(ENV.fetch("RAILSCOPE_ENABLED", nil))
+  end
 end
