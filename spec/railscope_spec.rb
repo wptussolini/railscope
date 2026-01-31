2e3e4c584687ecddf1455b5fbfe085e6f413dd26
Phelipe Tussolini <phelipe@taller.net.br>
2026-01-30 21:30:36 -0300

    first commit

---
+++ spec/railscope_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Railscope do
+  it "has a version number" do
+    expect(Railscope::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
